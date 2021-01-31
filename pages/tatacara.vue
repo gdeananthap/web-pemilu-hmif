34e2111131faf3c50b35f8c7c9fc9e7639c302c1
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2021-01-31 20:10:24 +0700

    Update tatacara.vue

--- pages/tatacara.vue
+++ pages/tatacara.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="page-content text-center">
-        <h1>Temporary Page</h1>
+        <h1>TATA CARA</h1>
+        <div class="box-first">
+            
+        </div>
     </div>
 </template>
 
@@ -10,5 +13,70 @@ export default {
 }
 </script>
 <style lang="scss">
+.box-first{
+    position: absolute;
+    width: 954px;
+    height: 100px;
+    left: 242px;
+    top: 342px;
+
+    border: 1px solid #000000;
+    box-sizing: border-box;
+    border-radius: 20px;
+}
+
+.box-next{
+    float: center;
+    width: 954px;
+    height: 100px;
+    left: 242px;
     
+    margin-top: 135px;
+
+    border: 1px solid #000000;
+    box-sizing: border-box;
+    border-radius: 20px;
+}
+
+/* Judul Utama */
+
+h1{
+    position: absolute;
+    width: 528px;
+    height: 110px;
+    left: 456px;
+    top: 204px;
+
+    font-family: Moon;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 96px;
+    line-height: 110px;
+    text-align: center;
+}
+
+/* Subjudul per Bab */
+h2{
+    font-family: Moon;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 41px;
+    display: flex;
+    align-items: center;
+
+    color: #000000;
+}
+
+/* Isi Bab */
+p{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 21px;
+
+    color: #000000;
+}
+
 </style>
