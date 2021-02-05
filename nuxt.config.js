b0150c7f38cf238df7d4c308fa814d143f59712b
margunwa123 <13518114@std.stei.itb.ac.id>
2021-02-05 18:01:01 +0700

    change database to pemiluhmif email

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,13 +57,13 @@ export default {
       "@nuxtjs/firebase",
       {
         config: {
-          apiKey: "AIzaSyA5eRtJcUX0VxOmlJzXjr3H_y8BSiZyM60",
-          authDomain: "pemilu-c3c44.firebaseapp.com",
-          projectId: "pemilu-c3c44",
-          storageBucket: "pemilu-c3c44.appspot.com",
-          messagingSenderId: "551940857191",
-          appId: "1:551940857191:web:f8d4bb242a684ce547a3c2",
-          measurementId: "G-0T0MY4TH8T",
+          apiKey: "AIzaSyC5V2PGCGXynULtkeRTMMcxs13wxPCd2HE",
+          authDomain: "pemilu-f561a.firebaseapp.com",
+          projectId: "pemilu-f561a",
+          storageBucket: "pemilu-f561a.appspot.com",
+          messagingSenderId: "1006768768666",
+          appId: "1:1006768768666:web:2b0c8c2195a21c7a994123",
+          measurementId: "G-VJK3F6DLE7",
           databaseURL: ""
         },
         services: {
