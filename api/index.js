c7a80bd356ab13bb2bbaf1dd71e29bf437000629
margunwa123 <13518114@std.stei.itb.ac.id>
2021-02-19 19:48:21 +0700

    hehe

--- api/index.js
+++ api/index.js
@@ -3,21 +3,24 @@ const express = require("express");
 const app = express();
 const serviceAccount = require("./utils/serviceAccountKey.json");
 const admin = require("firebase-admin");
-const firebase = require("firebase");
+const firebase = require("firebase").default;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-firebase.default.initializeApp({
-  apiKey: "AIzaSyC5V2PGCGXynULtkeRTMMcxs13wxPCd2HE",
-  authDomain: "pemilu-f561a.firebaseapp.com",
-  projectId: "pemilu-f561a",
-  storageBucket: "pemilu-f561a.appspot.com",
-  messagingSenderId: "1006768768666",
-  appId: "1:1006768768666:web:2b0c8c2195a21c7a994123",
-  measurementId: "G-VJK3F6DLE7"
-});
-
+if (!firebase.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+  firebase.initializeApp({
+    apiKey: "AIzaSyC5V2PGCGXynULtkeRTMMcxs13wxPCd2HE",
+    authDomain: "pemilu-f561a.firebaseapp.com",
+    projectId: "pemilu-f561a",
+    storageBucket: "pemilu-f561a.appspot.com",
+    messagingSenderId: "1006768768666",
+    appId: "1:1006768768666:web:2b0c8c2195a21c7a994123",
+    measurementId: "G-VJK3F6DLE7"
+  });
+} else {
+  firebase.app();
+}
 app.use(express.json());
 
 app.get("/", async (req, res) => {
