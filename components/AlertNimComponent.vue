dc1af852c14d12429722a7b42cb8f6b9c5a9fc06
margunwa123 <13518114@std.stei.itb.ac.id>
2021-02-20 17:19:37 +0700

    create danger alert

--- components/AlertNimComponent.vue
+++ components/AlertNimComponent.vue
@@ -3,23 +3,23 @@
     <div class="row justify-content-center">
       <div class="col-12 col-sm-8 col-md-6">
         <div
-          class="alert alert-danger"
-          v-if="isdpt === false && email !== null"
+          :class="
+            `alert alert-custom ${
+              isdpt ? 'alert-success-custom' : 'alert-danger-custom'
+            }`
+          "
+          role="alert"
         >
-          Anda bukan seorang dpt
-        </div>
-        <div
-          class="alert alert-success-custom"
-          v-if="isdpt === true && email !== null"
-        >
-          Anda adalah seorang dpt
-        </div>
-
-        <div class="alert alert-success-custom" role="alert">
           <div class="row">
             <div class="col-10 d-flex">
-              <p class="my-auto alert-message" v-if="email !== null">
-                Halo, {{ email }}
+              <p v-if="isdpt === false">
+                Anda bukan seorang dpt
+              </p>
+              <p
+                class="my-auto alert-message"
+                v-if="email !== null && isdpt === true"
+              >
+                Halo, {{ email }}, kamu merupakan dpt
               </p>
               <p class="my-auto alert-message" v-else>
                 Anda belum login, silahkan login dengan menakan tombol di pojok
@@ -64,10 +64,18 @@ export default {
 
 <style lang="scss">
 .alert-success-custom {
-  padding: 0.25rem 0.5rem;
-  color: white;
   background-color: #44b4c2;
   border-color: #44b4c2;
+}
+
+.alert-danger-custom {
+  background-color: #e45642;
+  border-color: #e45642;
+}
+
+.alert-custom {
+  padding: 0.25rem 0.5rem;
+  color: white;
   margin-bottom: 0px;
   margin-top: 25px;
   .alert-message {
